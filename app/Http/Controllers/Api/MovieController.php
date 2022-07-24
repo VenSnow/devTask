85e4439f8cdf52594a57882e55276fc7c387c46e
Tim Hale <timur3214r@gmail.com>
2022-07-24 16:10:41 +0500

    fix

--- app/Http/Controllers/Api/MovieController.php
+++ app/Http/Controllers/Api/MovieController.php
@@ -11,16 +11,22 @@ class MovieController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
      */
     public function index()
     {
-        return Movie::latest()->where('is_published', '=', '1')->paginate(15);
+        return Movie::with('genres')->latest()->where('is_published', '=', '1')->paginate(15);
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param $id
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model
+     */
     public function show($id)
     {
-        return Movie::findOrFail($id);
+        return Movie::with('genres')->findOrFail($id);
     }
 
 }
